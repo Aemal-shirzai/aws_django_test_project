909e528097843bc7923e3586d982cf3ec46a935f
aemal-shirzai <aemalshirzai2016@gmail.com>
2023-04-08 23:19:24 -0400

    [IMP] Add image field to todo.

--- main_project/todo_app/models.py
+++ main_project/todo_app/models.py
@@ -4,6 +4,7 @@ class Todo(models.Model):
 
     todo = models.TextField()
     completed = models.BooleanField(default=False)
+    todo_image = models.ImageField(upload_to="my_images", null=True)
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
